dcea1c6d7e371418027f74bfab3402e9f1705120
Ankit Ranjan <110593132+Ankitrj3@users.noreply.github.com>
2024-06-03 19:59:50 +0530

    Add files via upload

--- insertionarr.js
+++ insertionarr.js
@@ -1,14 +1,16 @@
 // insertion in array
 let arr = [10,234,21,34,12];
 function Insert(arr,index,element){
+    let arr1 = [];
     for(let i=0;i<arr.length;i++){
     //    arr.splice(index,0,element);
-    let arr1 = [];
-    if(arr[i]===index){
+    if(i===index){
         arr1.push(element);
-    }
+    }  
+    if(i<arr.length){
        arr1.push(arr[i]);
     }
+}
     return arr1;
 }
 Insert(arr,2,33);
